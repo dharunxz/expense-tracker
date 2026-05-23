c347384e3b6db7ac4dcdbcee31388a3db4923a00
DHARUN R <dharundon12345@gmail.com>
2026-05-23 19:44:07 +0530

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,2 +1,15 @@
-# expense-tracker
-Beginner project to track daily expenses and category-wise spending using Python/Excel.
+expenses = {
+    "Food": 500,
+    "Travel": 300,
+    "Shopping": 1200,
+    "Recharge": 250
+}
+
+total = sum(expenses.values())
+
+print("Total Expense:", total)
+
+print("\nCategory-wise Expenses:")
+
+for category, amount in expenses.items():
+    print(category, ":", amount)
